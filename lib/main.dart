b389eb480ae41feded36de3ca1bbe8d606896b8b
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2021-02-03 00:28:32 -0300

    Managing transferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/models/saldo.dart';
+import 'package:bytebank/models/transferencias.dart';
 import 'package:bytebank/screens/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -7,7 +8,10 @@ void main() => runApp(MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (context) => Saldo(0),
-        )
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Transferencias(),
+        ),
       ],
       child: BytebankApp(),
     ));
